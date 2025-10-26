348df981061cf7b79be7f014e13794a5cdca4ae7
Miguel Totti <migueltotti2005@gmail.com>
2025-10-26 17:28:39 -0300

    feat: add result pattern

---
+++ CorteCerto.Domain/Base/Error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorteCerto.Domain.Base;
+
+public sealed record Error(string Code, string Description)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
