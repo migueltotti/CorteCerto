81f5ce632ff61545c210ffd2950dd2f282b83d91
Miguel Totti <migueltotti2005@gmail.com>
2026-02-24 23:54:59 -0300

    refactor: update ViaCepResponse properties name and configure json serializer properties name

--- src/CorteCerto.Infrastructure/DTO/ViaCepResponse.cs
+++ src/CorteCerto.Infrastructure/DTO/ViaCepResponse.cs
@@ -2,26 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CorteCerto.Infrastructure.DTO;
 
 internal sealed class ViaCepResponse
 {
-    public string? Cep { get; set; }
-    public string? Logradouro { get; set; }
-    public string? Complemento { get; set; }
-    public string? Unidade { get; set; }
-    public string? Bairro { get; set; }
-    public string? Localidade { get; set; }
+    [JsonPropertyName("Cep")]
+    public string? Zip { get; set; }
+    
+    [JsonPropertyName("Logradouro")]
+    public string? PublicPlace { get; set; }
+    
+    [JsonPropertyName("Complemento")]
+    public string? Complement { get; set; }
+    
+    [JsonPropertyName("Unidade")]
+    public string? Unity { get; set; }
+    
+    [JsonPropertyName("Bairro")]
+    public string? District { get; set; }
+    
+    [JsonPropertyName("Localidade")]
+    public string? Location { get; set; }
+    
+    [JsonPropertyName("Uf")]
     public string? Uf { get; set; }
-    public string? Estado { get; set; }
-    public string? Regiao { get; set; }
+    
+    [JsonPropertyName("Estado")]
+    public string? State { get; set; }
+    
+    [JsonPropertyName("Regiao")]
+    public string? Region { get; set; }
+    
+    [JsonPropertyName("Ibge")]
     public string? Ibge { get; set; }
+    
+    [JsonPropertyName("Gia")]
     public string? Gia { get; set; }
+    
+    [JsonPropertyName("DDD")]
     public string? DDD { get; set; }
+    
+    [JsonPropertyName("Siafi")]
     public string? Siafi { get; set; }
-    public string? Erro { get; set; }
+    
+    [JsonPropertyName("Erro")]
+    public string? Error { get; set; }
 
-    public bool IsValid() => Erro is null || Boolean.Parse(Erro) == false;
+    public bool IsValid() => Error is null || Boolean.Parse(Error) == false;
 }
